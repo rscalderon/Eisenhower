4b61cf6cac3441f7edf4038ce2fd20f44e6684f8
rscalderon <samourcalderon@gmail.com>
2023-12-10 17:53:05 -0500

    final cleanup MainContainer component library

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,57 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import Quadrant from '../components/Quadrant.jsx';
-import { redirect } from 'react-router-dom';
 
-// Builds four Quadrant components with their corresponding tasks
+// Fetch tasks from database and build four Quadrant components
 const MainContainer = () => {
-  // init allQuadrants as state and updateQuadrants as setter function
+  // define array of quadrants as state
   const [allQuadrants, updateQuadrants] = useState([]);
-  // init messageArr, an array that contains the headers of our four Quadrants
-  const messageArr = [
-    'Important, Urgent',
-    'Important, not Urgent',
-    'Urgent, not Important',
-    'Not Urgent, not Important',
-  ];
-  // init const quadrantsArr, the array that will hold our quadrant components, as an empty arr
-  const quadrantsArr = [];
   // fetch tasks and update state to array with 4 quadrant components
   useEffect(() => {
-    // taskList is an array of arrays that contain the tasks of each quadrant
-    const taskList = [];
+    const quadrantHeaders = [
+      'Important, Urgent',
+      'Important, not Urgent',
+      'Urgent, not Important',
+      'Not Urgent, not Important',
+    ];
+    // Nested array that stores the tasks of each quadrant
+    const allTasks = [];
     // Fetch tasks from database and build array with 4 quadrant components
     const fetchTasks = async () => {
-      for (let i = 0; i < messageArr.length; i++) {
-        // init const tasks as the result of taskFetch on the index of each message in messageArr
+      // temporary store of quadrant components before updating state
+      const quadrantsArr = [];
+      for (let i = 0; i < quadrantHeaders.length; i++) {
         try {
-          const response = await fetch(`/tasks?priority=${quadrantNum}`);
-          const tasks = await response.json();
-          redirect('/');
-          taskList.push(tasks);
-          return tasks;
+          // store each quadrant's tasks retrieved from database and push to allTasks array
+          const tasks = await fetch(`/tasks?priority=${i}`).json();
+          allTasks.push(tasks);
         } catch (err) {
           console.error('Error at taskCreator in App.jsx:', err);
         }
-        // push each new Quadrant component into the quadrantsArr
+        // populate quadrantsArr with Quadrant components
         quadrantsArr.push(
           <Quadrant
             id={i + 1}
             key={`Q${i + 1}`}
             className='quadrant'
-            message={messageArr[i]}
+            message={quadrantHeaders[i]}
             tasks={tasks}
             setter={updateQuadrants}
           ></Quadrant>
         );
-        // re-render MainContainer component
+        // re-render MainContainer component with all quadrants
         updateQuadrants(quadrantsArr);
       }
     };
-    // invoke fetchTasks
     fetchTasks();
   }, []);
   return (
-    <main id='AppContainer'>{allQuadrants ? allQuadrants : 'Loading...'}</main>
+    <main id='AppContainer'>
+      {allQuadrants ? allQuadrants : 'Loading...'.repeat(4)}
+    </main>
   );
 };
 
